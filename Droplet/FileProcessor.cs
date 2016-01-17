2214bc224a54181d4a5991b560965a2ebadf2dec
Greg Bell <nzwebguru@gmail.com>
2016-01-17 09:46:01 +0800

    Initial commit

---
+++ Droplet/FileProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using ExifLibrary;
+
+namespace PhotoDateCorrector
+{
+	public static class PhotoProcessor
+	{
+		public static void ProcessImage(String filePath)
+		{
+
+		}
+	}
+}
---
+++ Droplet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Droplet
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		public static void Main(String[] filePaths)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new ProgressDialog());
+		}
+	}
+}
