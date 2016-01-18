0d30fd40673faa9b53fe45b79c03519ee5f5324d
Greg Bell <nzwebguru@gmail.com>
2016-01-18 08:15:17 +0800

    Starting work on enabling the progress dialog.

--- Droplet/ProgressDialog.cs
+++ Droplet/ProgressDialog.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PhotoDateCorrector;
 
 namespace Droplet
 {
 	public partial class ProgressDialog : Form
 	{
+		BackgroundWorker backgroundWorker;
+
 		public ProgressDialog()
 		{
 			InitializeComponent();
@@ -19,7 +22,10 @@ namespace Droplet
 
 		public ProgressDialog(String[] filePaths)
 		{
-
+			foreach (var path in filePaths)
+			{
+				PhotoProcessor.ProcessImage(path);
+			}
 		}
 	}
 }
