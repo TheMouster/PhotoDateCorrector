3e33f512bcee54d207181407872a0f7aa3387260
Greg Bell <classified.development@gmail.com>
2018-02-17 16:38:13 +0800

    Added checks for the GPSDate being incorrectly set to DateTime.MinValue. Also capture of exceptions and logging to the Application log.

--- Droplet/PhotoProcessor.cs
+++ Droplet/PhotoProcessor.cs
@@ -3,12 +3,15 @@ using System.IO;
 using ExifLibrary;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace PhotoDateCorrector
 {
 	public static class PhotoProcessor
 	{
-		public static void ProcessImages(String[] filePaths)
+        const String APPLICATION_LOG = "Application";
+
+        public static void ProcessImages(String[] filePaths)
 		{
 			/*
 			foreach (var path in filePaths)
@@ -32,12 +35,14 @@ namespace PhotoDateCorrector
 			{
 				whenDigitised = (DateTime)image.Properties[ExifTag.DateTimeDigitized].Value;
 			}
-			catch(KeyNotFoundException) //EXIF data doesn't contain a DateTimeDigitized flag
+			catch(KeyNotFoundException kex) //EXIF data doesn't contain a DateTimeDigitized flag
 			{
-				return;
+                EventLog.WriteEntry(APPLICATION_LOG, FormatMessage("Likely missing DateTimeDigitized EXIF tag." + kex.Message), EventLogEntryType.Error);
+                return;
 			}
 			
 			//Correct the EXIF values in the image and save it.
+
 			try
 			{
 				image.Properties[ExifTag.DateTime].Value = whenDigitised;
@@ -68,10 +73,37 @@ namespace PhotoDateCorrector
 				image.Properties.Add(ExifTag.ThumbnailDateTime, thumbnailDate);
 			}
 
-			image.Save(filePath);
-			
-			//Update the images' file creation time.
-			File.SetCreationTime(filePath, whenDigitised);
-		}
+            var correctedGPSDate = new DateTime(whenDigitised.Year, whenDigitised.Month, whenDigitised.Day);
+            try
+            {
+                var gpsDate = (DateTime)image.Properties[ExifTag.GPSDateStamp].Value;
+                if (gpsDate == DateTime.MinValue)
+                {
+                    image.Properties[ExifTag.GPSDateStamp].Value = correctedGPSDate;
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                var gpsDate = new ExifDateTime(ExifTag.GPSDateStamp, correctedGPSDate);
+                image.Properties.Add(ExifTag.GPSDateStamp,gpsDate);
+            }
+
+            try
+            {
+                image.Save(filePath);
+
+                //Update the images' file creation time.
+                File.SetCreationTime(filePath, whenDigitised);
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(APPLICATION_LOG, FormatMessage(ex.Message), EventLogEntryType.Error);
+            }
+        }
+
+        private static String FormatMessage(String message)
+        {
+            return "PhotoDateCorrector: " + message;
+        }
 	}
 }
